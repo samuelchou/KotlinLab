48b5f488928be77f7a5bae418948f9f6e6fab8eb
samuelchou <samuelchou@hotmail.com.tw>
2020-10-14 23:02:32 +0800

    初步嘗試賦值操作。

--- app/src/main/java/studio/ultoolapp/kotlinlab/MainActivity.kt
+++ app/src/main/java/studio/ultoolapp/kotlinlab/MainActivity.kt
@@ -1,11 +1,14 @@
 package studio.ultoolapp.kotlinlab
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val helloWorld = "Hello WALUDO!"
+        findViewById<TextView>(R.id.titleText).text = helloWorld
     }
 }
