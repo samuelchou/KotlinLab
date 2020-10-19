74f22e2d41d4c156b7a51cbdee1a7e78c74dfa3b
samuelchou <samuelchou@hotmail.com.tw>
2020-10-19 16:31:14 +0800

    <Add> 初步完成BMI計算功能。

--- app/src/main/java/studio/ultoolapp/kotlinlab/ui/ActivityBmi.kt
+++ app/src/main/java/studio/ultoolapp/kotlinlab/ui/ActivityBmi.kt
@@ -2,11 +2,19 @@ package studio.ultoolapp.kotlinlab.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_bmi.*
 import studio.ultoolapp.kotlinlab.R
 
 class ActivityBmi : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bmi)
+        btnCalculate.setOnClickListener {
+            val weight = editTextWeight.text.toString().toFloat()
+            val height = editTextHeight.text.toString().toFloat()
+            val bmi = weight / (height * height)
+            textMessage.text = "Your BMI is: $bmi"
+        }
+
     }
 }
