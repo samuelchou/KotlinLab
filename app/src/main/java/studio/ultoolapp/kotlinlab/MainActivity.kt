a7859255b30c83682df8d0beb70a079f467da5e9
samuelchou <samuelchou@hotmail.com.tw>
2020-10-19 16:15:43 +0800

    <Add> 現在MainActivity點擊後會導引到ActivityBmi了。

--- app/src/main/java/studio/ultoolapp/kotlinlab/MainActivity.kt
+++ app/src/main/java/studio/ultoolapp/kotlinlab/MainActivity.kt
@@ -1,14 +1,20 @@
 package studio.ultoolapp.kotlinlab
 
+import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import studio.ultoolapp.kotlinlab.ui.ActivityBmi
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val helloWorld = "Hello WALUDO!"
-        findViewById<TextView>(R.id.titleText).text = helloWorld
+        titleText.text = helloWorld
+        titleText.setOnClickListener {
+            val intent = Intent(this, ActivityBmi::class.java)
+            startActivity(intent)
+        }
     }
 }
