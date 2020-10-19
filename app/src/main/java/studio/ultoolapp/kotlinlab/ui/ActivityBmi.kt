623e5bb9f87b4ad598a0dbab09344df58897f914
samuelchou <samuelchou@hotmail.com.tw>
2020-10-19 16:15:07 +0800

    <Add> 新增BMI Activity.

---
+++ app/src/main/java/studio/ultoolapp/kotlinlab/ui/ActivityBmi.kt
@@ -0,0 +1,12 @@
+package studio.ultoolapp.kotlinlab.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import studio.ultoolapp.kotlinlab.R
+
+class ActivityBmi : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bmi)
+    }
+}
