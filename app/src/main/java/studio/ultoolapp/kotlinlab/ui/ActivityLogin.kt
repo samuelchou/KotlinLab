44e1f3dda385cdf050d19764d5fe2aa261aba6a5
samuelchou <samuelchou@hotmail.com.tw>
2020-10-23 00:22:53 +0800

    <Add> ActivityLogin 提供初步登入設計。

--- app/src/main/java/studio/ultoolapp/kotlinlab/ui/ActivityLogin.kt
+++ app/src/main/java/studio/ultoolapp/kotlinlab/ui/ActivityLogin.kt
@@ -1,12 +1,25 @@
 package studio.ultoolapp.kotlinlab.ui
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
 import studio.ultoolapp.kotlinlab.R
 
 class ActivityLogin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        btnLogin.setOnClickListener {
+            login()
+        }
+    }
+
+    private fun login() {
+        if (editUserName.text.toString() == "Email" && editPassword.text.toString() == "Password") {
+            finish()
+        } else {
+            Toast.makeText(this, "Wrong Email or Password", Toast.LENGTH_SHORT).show()
+        }
     }
 }
