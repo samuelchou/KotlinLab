862b007a1074a06cd70815103a45ead32b747521
samuelchou <samuelchou@hotmail.com.tw>
2020-10-23 23:43:45 +0800

    <Add> 新增 ListSetMapTest, 測試各種集合內容。

---
+++ app/src/test/java/studio/ultoolapp/kotlinlab/ListSetMapTest.kt
@@ -0,0 +1,38 @@
+package studio.ultoolapp.kotlinlab
+
+import org.junit.Test
+
+class ListSetMapTest {
+    @Test
+    fun list_test() {
+        val list = mutableListOf<Int>()
+        list.add(350)
+        list[0] += 30
+        println(list[0])
+    }
+
+    @Test
+    fun set_test() {
+        val girls = setOf("Bonnie", "Anna", "Cynthia")
+        for (name in girls) {
+            println("We have $name here")
+        }
+
+        val boys = setOf("Dan", "Edison")
+        for ((i, v) in boys.withIndex()) {
+            println("No.${i + 1} is $v")
+        }
+    }
+
+    @Test
+    fun map_test() {
+        val grades = mutableMapOf("Daisy" to 65, "Sam" to 85)
+        grades["X Æ A-12"] = 100
+        grades["Daisy"] = grades["Daisy"]!! + 10
+        // grades["Ellon"] = grades["Ellon"]!! + 10 // NPE
+        for ((n, g) in grades) {
+            println("$n's grade is $g")
+        }
+    }
+
+}
