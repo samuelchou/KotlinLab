7048cc4e09eb5479a9d6ede997cf29421e9f0880
samuelchou <samuelchou@hotmail.com.tw>
2020-10-23 16:52:30 +0800

    <Add> ActivityList: 引入RecyclerView, 初步完成版面配置, 並提供初階Adapter.

---
+++ app/src/main/java/studio/ultoolapp/kotlinlab/ui/ActivityList.kt
@@ -0,0 +1,54 @@
+package studio.ultoolapp.kotlinlab.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_list_example.view.*
+import studio.ultoolapp.kotlinlab.R
+import java.util.*
+
+class ActivityList : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var recyclerAdapter: RecyclerView.Adapter<*>
+    private var items = ArrayList<String>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list)
+        recyclerAdapter = object : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+            override fun onCreateViewHolder(
+                parent: ViewGroup,
+                viewType: Int
+            ): RecyclerView.ViewHolder {
+                val view = LayoutInflater.from(this@ActivityList)
+                    .inflate(R.layout.item_list_example, parent, false)
+                return object : RecyclerView.ViewHolder(view) {}
+            }
+
+            override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+                holder.itemView.itemTitle.text = items.get(position)
+            }
+
+            override fun getItemCount(): Int {
+                return items.size
+            }
+        }
+        items.add("John")
+        items.add("Adam")
+        items.add("Bill")
+        items.add("Coco")
+
+        recyclerView = findViewById<RecyclerView>(R.id.recyclerView).apply {
+            setHasFixedSize(true)
+
+            // use a linear layout manager
+            layoutManager = LinearLayoutManager(this@ActivityList)
+
+            adapter = recyclerAdapter
+        }
+
+    }
+}
