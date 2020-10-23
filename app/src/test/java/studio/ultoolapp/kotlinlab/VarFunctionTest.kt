a060970bdebfaa887e97d2dbd61ebafe4e75d8c3
samuelchou <samuelchou@hotmail.com.tw>
2020-10-23 23:12:20 +0800

    <Add> 新增VarFunctionTest, 測試基礎變數與函式功能。

---
+++ app/src/test/java/studio/ultoolapp/kotlinlab/VarFunctionTest.kt
@@ -0,0 +1,46 @@
+package studio.ultoolapp.kotlinlab
+
+import org.junit.Test
+
+class VarFunctionTest {
+    @Test
+    fun var_and_val() {
+        var rank = 3;
+        val value = 10
+        println("Rank is $rank, Value is $value")
+        rank = 6
+        println("Rank is $rank, Value is $value")
+        // value = 4 // Error: Value cannot be assigned
+        // println("Rank is $rank, Value is $value")
+    }
+
+    @Test
+    fun nullable_deal() {
+        var s1: String? = "10.25"
+        println(s1!!.toDouble()) // 10.25
+
+        var s2: String = s1
+        println(s2.toDouble()) // 10.25
+
+        s1 = null // No Exception: s1 is nullable String
+        println(s1) // Print null
+        // println(s1!!.toDouble()) // NullPointerException
+
+        fun greetings(name: String?) = "Hello, $name"
+
+        println(greetings(s2)) // Hello, 10.25
+        println(greetings(s1)) // Hello, null
+
+        s2.toUpperCase()
+        // s1.toUpperCase() // unacceptable: not for nullable String
+
+        // s2 = s1!! // NullPointerException
+    }
+
+    @Test
+    fun print_fun() {
+        fun addTwo(a: Int, b: Int) = a + b
+        println("1 + 2 is ${addTwo(1, 2)}")
+    }
+
+}
